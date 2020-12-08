7eae3043b2913582bbbb9b220f4cb33dc743bc2d
trainingdox <75624739+trainingdox@users.noreply.github.com>
2020-12-08 21:26:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'sammy';
-const MONGO_PASSWORD = 'password';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'sharkinfo';
+const { MONGO_USERNAME,
+MONGO_PASSWORD,
+MONGO_HOSTNAME,
+MONGO_PORT,
+MONGO_DB
+} = process.env;
+
+const options = {
+        useNewUrlParser: true,
+        reconnectTries: 30,
+        reconnectInterval: 500,
+        connectionTimeoutMS: 100000,
+};
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, options).then( function(){
+        console.log('Mongo connect');
+})
+        .catch(function(err)){
+        console.log(err);
+});
